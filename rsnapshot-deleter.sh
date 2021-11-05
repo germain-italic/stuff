7c29571f5f4d8f86500c515036eabbd4cec9ae18
Germain <germain@italic.fr>
2021-11-05 06:34:52 +0100

    rsnapshot recursive delete script

--- rsnapshot-deleter.sh
+++ rsnapshot-deleter.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
 RSDIR="/home/rsnapshot"
-BPDIR="ouikili.com"
-INTERVAL="daily"
+BKPOINTS="daily.0"
 
-for i in {1..5}
+
+
+echo -e "\nWHICH BACKUP POINT WOULD YOU LIKE TO DELETE?"
+echo -e "(Please type only one)"
+
+for BKPOINT in "${RSDIR}/${BKPOINTS}/"
 do
-   echo "Welcome $i times"
+   ls $BKPOINT
+done
+
+read DELETE
+
+echo -e "\nDELETING $DELETE"
+
+INTERVALS=`/bin/grep -i "retain" /etc/rsnapshot.conf | /usr/bin/awk '{print $2}'`
+for INTERVAL in ${INTERVALS}
+do
+
+   RETAINS=`/bin/grep -iP "retain\t${INTERVAL}" /etc/rsnapshot.conf | /usr/bin/awk '{print $3}'`
+   echo -e "\nINTERVAL ${INTERVAL} (${RETAINS} copies):"
+
+   for i in $(/usr/bin/seq 0 $RETAINS)
+   do
+      PATH="${RSDIR}/${INTERVAL}.${i}/${DELETE}"
+      if [ -d $PATH ] 
+      then
+         # SIZE=`/usr/bin/find $PATH -exec /usr/bin/du -s {} + | /usr/bin/awk '{total = total + $1}END{print (total / 1024 / 1024) "MB"}'`
+         # echo "    $PATH $SIZE" 
+         echo "    $PATH" 
+         /bin/rm -rf $PATH
+      else
+         echo "    ERROR: THE BACKUP POINT $PATH DOES NOT EXIST." 
+         # exit 1
+      fi
+   done
+
 done
