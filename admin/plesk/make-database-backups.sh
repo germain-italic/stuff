4233b8ac8b805ba0d19d356ea9197d14c8b87d5f
Germain Metti <germain@italic.fr>
2022-04-02 08:35:56 +0200

    backup plesk databases

---
+++ admin/plesk/make-database-backups.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOCAL_DEST="/root/mysql_dumps_all"
+REMOTE_DEST="/home/germain/Tél"échargements/mysql_dumps_all""
+
+mkdir -p $LOCAL_DEST
+
+/usr/sbin/plesk db -e "show databases" | \
+    grep -v -E "^Database|information_schema|performance_schema|phpmyadmin" > "$LOCAL_DEST"/dblist.txt
+
+cat "$LOCAL_DEST"/dblist.txt | while read i; \
+    do /usr/sbin/plesk db dump "$i" > "$LOCAL_DEST"/"$i".sql; \
+done;
+
+scp -r $LOCAL_DEST germain@ip:"$REMOTE_DEST"
