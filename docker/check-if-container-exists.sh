6950d788dd31b0d6a2c60fa6a593a805d4125a51
Germain Metti <germain@italic.fr>
2022-11-04 08:40:55 +0400

    docker - script to check if container exists

---
+++ docker/check-if-container-exists.sh
@@ -0,0 +1,12 @@
+CONTAINER=gitlab
+docker inspect --format="{{.State.Running}}" $CONTAINER
+if [ $? -eq 0 ];
+then
+    echo "container gitlab exists"
+else
+    echo "missing container gitlab"
+    MESSAGE="Il n'y a pas de container intitulé 'gitlab' sur xxx,\n"
+    MESSAGE="${MESSAGE}le cron de backup ne peut donc pas s'exécuter !\n"
+    MESSAGE="${MESSAGE}Lancez Portainer pour inspecter : http://xxx:xxx/#!/auth"
+    echo -e "Subject:!!! ALERTE CONTAINER GITLAB MANQUANT !!!\n$MESSAGE" | /usr/sbin/sendmail xxx
+fi
